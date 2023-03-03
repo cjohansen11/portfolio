a0a75a01d6bbd61f19e4eaff6e1cc49a5ea4a8d9
cjohansen11 <cjohansen11@me.com>
2023-03-03 15:25:40 -0500

    Changed sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,40 +3,42 @@
 
 <div class="flex h-screen flex-col">
 	<div class="m-auto text-center">
-		<h1 class="md:text-7xl sm:text-6xl xs:text-3xl">Christian Johansen</h1>
-		<h6 class="md:text-5xl sm:text-4xl xs:text-2xl font-thin xs:font-light">
+		<h1 class="lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl sm:font-medium">
+			Christian Johansen
+		</h1>
+		<h6
+			class="lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl font-thin sm:font-light xs:font-light"
+		>
 			Full Stack Developer
 		</h6>
-		<p class="lg:text-lg md:text-base sm:text-sm xs:text-xs font-thin xs:font-light">
-			Website coming soon
-		</p>
-		<div class="lg:mt-24 mt-12 xs:mt-8 flex justify-center gap-8 xs:gap-4">
+		<p class="lg:text-base md:text-sm sm:text-xs xs:text-xs font-thin">Website coming soon</p>
+		<div class="lg:mt-18 mt-10 xs:mt-8 flex justify-center gap-8 xs:gap-4">
 			<a href="https://www.github.com/cjohansen11" target="_blank" rel="noreferrer">
 				<img
 					src="/icons/icon_github.svg"
 					alt="github icon"
-					class="m-0 lg:h-12 lg:w-12 md:h-8 md:w-8 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
 				/>
 			</a>
 			<a href="https://www.linkedin.com/in/christian-johansen11" target="_blank" rel="noreferrer">
 				<img
 					src="/icons/icon_linkedin.svg"
 					alt="linkedin icon"
-					class="m-0 lg:h-12 lg:w-12 md:h-8 md:w-8 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
 				/>
 			</a>
 			<a href="mailto: cjohansen11@me.com">
 				<img
 					src="/icons/icon_email.svg"
 					alt="email icon"
-					class="m-0 lg:h-12 lg:w-12 md:h-8 md:w-8 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
 				/>
 			</a>
 			<a href="/CJohansen_Resume.pdf" target="_blank" rel="noreferrer">
 				<img
 					src="/icons/icon_download.svg"
 					alt="download icon"
-					class="m-0 lg:h-12 lg:w-12 md:h-8 md:w-8 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
 				/>
 			</a>
 		</div>
