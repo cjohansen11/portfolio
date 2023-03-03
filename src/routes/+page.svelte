5e14c97e43b685eda8272908a0cee14907fea927
cjohansen11 <cjohansen11@me.com>
2023-03-03 10:23:49 -0500

    Added linkedin icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,13 @@
 	<div class="m-auto text-center">
 		<h1>Christian Johansen</h1>
 		<h6>Full Stack Developer</h6>
-		<p>Website coming soon</p>
+		<p class="text-lg">Website coming soon</p>
 		<div class="mt-12 flex justify-center gap-8">
 			<a href="https://www.github.com/cjohansen11" target="_blank" rel="noreferrer">
 				<img src="/icons/icon_github.svg" alt="github icon" class="m-0 h-12 w-12" />
+			</a>
+			<a href="https://www.linkedin.com/in/christian-johansen11" target="_blank" rel="noreferrer">
+				<img src="/icons/icon_linkedin.svg" alt="linkedin icon" class="m-0 h-12 w-12 " />
 			</a>
 			<a href="mailto: cjohansen11@me.com">
 				<img src="/icons/icon_email.svg" alt="email icon" class="m-0 h-12 w-12 " />
