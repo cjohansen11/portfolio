027c6895da2c4fc00d6cdec8401cfabdb75d55f9
cjohansen11 <cjohansen11@me.com>
2023-08-03 13:20:20 -0400

    Added new portfolio route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,70 +3,65 @@
 	import { fade } from 'svelte/transition';
 
 	trackPageView();
-
-	let visible = true;
 </script>
 
-{#if visible}
-	<div class="flex h-screen flex-col" transition:fade>
-		<div class="m-auto text-center">
-			<h1 class="lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl sm:font-medium">
-				Christian Johansen
-			</h1>
-			<h6
-				class="lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl font-thin sm:font-light xs:font-light"
-			>
-				Full Stack Developer
-			</h6>
-			<div class="lg:mt-18 mt-10 xs:mt-8 flex justify-center gap-8 xs:gap-4">
-				<a
-					href="https://www.github.com/cjohansen11"
-					target="_blank"
-					rel="noreferrer"
-					on:click={() => trackIconClick('github')}
-				>
-					<img
-						src="/icons/icon_github.svg"
-						alt="github icon"
-						class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
-					/>
-				</a>
-				<a
-					href="https://www.linkedin.com/in/christian-johansen11"
-					target="_blank"
-					rel="noreferrer"
-					on:click={() => trackIconClick('linkedin')}
-				>
-					<img
-						src="/icons/icon_linkedin.svg"
-						alt="linkedin icon"
-						class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
-					/>
-				</a>
-				<a href="mailto: cjohansen11@me.com" on:click={() => trackIconClick('email')}>
-					<img
-						src="/icons/icon_email.svg"
-						alt="email icon"
-						class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
-					/>
-				</a>
-				<a
-					href="/CJohansen_Resume.pdf"
-					target="_blank"
-					rel="noreferrer"
-					on:click={() => trackIconClick('resume')}
-				>
-					<img
-						src="/icons/icon_download.svg"
-						alt="download icon"
-						class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
-					/>
-				</a>
-			</div>
-		</div>
-		<button
-			style="rotate: 180deg; color: white; font-size: 18px; margin-bottom: 8px"
-			on:click={() => (visible = false)}>^</button
+<div class="flex h-screen flex-col" transition:fade>
+	<div class="m-auto text-center">
+		<h1 class="lg:text-5xl md:text-4xl sm:text-3xl xs:text-2xl sm:font-medium">
+			Christian Johansen
+		</h1>
+		<h6
+			class="lg:text-4xl md:text-3xl sm:text-2xl xs:text-xl font-thin sm:font-light xs:font-light"
 		>
+			Full Stack Developer
+		</h6>
+		<div class="lg:mt-18 mt-10 xs:mt-8 flex justify-center gap-8 xs:gap-4">
+			<a
+				href="https://www.github.com/cjohansen11"
+				target="_blank"
+				rel="noreferrer"
+				on:click={() => trackIconClick('github')}
+			>
+				<img
+					src="/icons/icon_github.svg"
+					alt="github icon"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+				/>
+			</a>
+			<a
+				href="https://www.linkedin.com/in/christian-johansen11"
+				target="_blank"
+				rel="noreferrer"
+				on:click={() => trackIconClick('linkedin')}
+			>
+				<img
+					src="/icons/icon_linkedin.svg"
+					alt="linkedin icon"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+				/>
+			</a>
+			<a href="mailto: cjohansen11@me.com" on:click={() => trackIconClick('email')}>
+				<img
+					src="/icons/icon_email.svg"
+					alt="email icon"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+				/>
+			</a>
+			<a
+				href="/CJohansen_Resume.pdf"
+				target="_blank"
+				rel="noreferrer"
+				on:click={() => trackIconClick('resume')}
+			>
+				<img
+					src="/icons/icon_download.svg"
+					alt="download icon"
+					class="m-0 lg:h-8 lg:w-8 md:h-6 md:w-6 sm:h-6 sm:w-6 xs:h-6 xs:w-6"
+				/>
+			</a>
+		</div>
 	</div>
-{/if}
+	<a style="rotate: 180deg; color: white; font-size: 18px; margin-bottom: 8px" href="/portfolio"
+		>^</a
+	>
+</div>
---
+++ src/routes/portfolio/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+</script>
+
+<div transition:fade>
+	<h1>Portfolio</h1>
+</div>
